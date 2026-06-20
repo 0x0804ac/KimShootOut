6249adf46c104ad1d90c79a50532b30a2359922f
Nuonlee <99leesg@gmail.com>
2026-06-20 12:37:24 +0900

    [ADD] add manager scripts
    add Gamemanager and Scenemanager scripts

---
+++ ShootOut/Assets/Scripts/Manager/SceneLoader.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader: MonoBehaviour
+{
+    public string scenename;
+
+    public void callScene(string name){
+        scenename = name;
+        SceneManager.LoadScene(scenename);
+    }
+    public void loadMainmenu()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void loadMultigame()
+    {
+        SceneManager.LoadScene("Multi");
+    }
+    public void loadCampaign()
+    {
+        SceneManager.LoadScene("Campaign");
+    }
+    public void loadAchivement()
+    {
+        SceneManager.LoadScene("Achivement");
+    }
+    public void loadCollectin()
+    {
+        SceneManager.LoadScene("Collections");
+    }
+    public void loadCustomize()
+    {
+        SceneManager.LoadScene("Customize");
+    } 
+    public string GetcurrentScene(){ return scenename }
+}
